cf68ee623c80f2f920d5d19580e33072d00402ef
ck-js <ckentertainmentinfo@gmail.com>
2024-02-04 18:40:12 +0700

    add task item and create task items component and export to index js to render to dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './style.css'
+import {
+    taskItemsContainer,
+createTaskItem,
+
+} from './user-interface'
+
 
 // all dom related functions go here
-
-
+// append task items container to output
 const output = document.getElementById('output')
-output.innerHTML = 'hello'
+output.appendChild(taskItemsContainer())
+// append task items to task items container
+const taskItemsParent = document.getElementById('task-items-container')
+taskItemsParent.appendChild(createTaskItem())
--- src/user-interface.js
+++ src/user-interface.js
@@ -1,6 +1,6 @@
 
 // array to store task item elements
-taskItemsArray = [];
+let taskItemsArray = ['Task1', 'Task2','Task3'];
 
 
 // create task items container 
@@ -8,6 +8,30 @@ function taskItemsContainer() {
     const container = document.createElement('div')
     container.id = 'task-items-container'
 
+return container;
+}
+// create task item
+function createTaskItem() {
+    const container = document.createElement('div')
+    for (let i = 0;i < taskItemsArray.length; i++) {
+        const item = taskItemsArray[i];
+        
+        const div = document.createElement('div');
+        div.textContent = item;
+
+
+console.log(item);
+        container.appendChild(div)
+    }
+    return container;
+
 
 }
 
+
+
+export {
+taskItemsContainer,
+createTaskItem,
+
+}
